7011ae70ed2e4b814e1a6ee9a42e09f27d2e7a62
shivam singh yadav <98205615+00develops@users.noreply.github.com>
2024-08-31 16:59:43 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <section className="p-10 bg-emerald-200 flex justify-center">
             <p className="font-bold">
-                ©Copyright 2023. All rights are reserved by SHIVAM
+                ©Copyright 2024. All rights are reserved by SHIVAM
             </p>
         </section>
     );
